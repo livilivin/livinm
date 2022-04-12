91cbf8aac93c47bb03b444d1b12a4d31a066c21c
livilivin <livinponnu1234@gmail.com>
2022-04-12 19:28:27 +0500

    ari added

---
+++ ari.sh
@@ -0,0 +1,12 @@
+echo"ARITHMETIC OPERATIONS"
+echo"enter 2 numbers"
+read a b
+s=`expr $a + $b`
+su=`expr $a - $b`
+p=`expr $a * $b`
+div=`expr $a / $b`
+echo"sum is $s"
+echo"subtraction is $su"
+echo"product is $p"
+echo"division is $div"
+
